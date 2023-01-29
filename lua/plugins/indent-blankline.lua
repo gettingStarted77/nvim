8bd4ee6c4331482d3ad64076eee90f332cfe1924
im <ispingzhou@gmail.com>
2023-01-29 10:40:34 +0800

    VeryLazy load it for better startup time

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,5 +1,6 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    event = "VeryLazy",
     init = function()
         vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
         vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
