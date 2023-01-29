0ad088f88901b36a7cc11e576f35dd112c5a20e6
im <ispingzhou@gmail.com>
2023-01-29 17:28:02 +0800

    beautify bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,28 @@
 return {
     "akinsho/bufferline.nvim",
     version = "v3.*",
-    config = true,
+    opts = {
+        options = {
+            mode = "buffers", -- set to "tabs" to only show tabpages instead
+            separator_style = "slant",
+            numbers = function(opts)
+                return string.format('%s·%s', opts.id, opts.raise(opts.ordinal))
+            end,
+            hover = {
+                enabled = true,
+                delay = 200,
+                reveal = { 'close' }
+            },
+            indicator = {
+                icon = '▎', -- this should be omitted if indicator style is not 'icon'
+                style = 'underline',
+            },
+            buffer_close_icon = '',
+            modified_icon = '●',
+            close_icon = '',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+        },
+    },
     dependencies = "nvim-tree/nvim-web-devicons",
 }
